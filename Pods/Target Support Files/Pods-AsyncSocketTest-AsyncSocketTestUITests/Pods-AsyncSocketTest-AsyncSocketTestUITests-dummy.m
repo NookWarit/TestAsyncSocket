311d28a340147ab8165a246a93d93373a989b8a7
Foodstory <foodstory@Waritthas-MBP.localdomain>
2021-02-22 14:25:07 +0700

    add pod asyncsocket

---
+++ Pods/Target Support Files/Pods-AsyncSocketTest-AsyncSocketTestUITests/Pods-AsyncSocketTest-AsyncSocketTestUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AsyncSocketTest_AsyncSocketTestUITests : NSObject
+@end
+@implementation PodsDummy_Pods_AsyncSocketTest_AsyncSocketTestUITests
+@end
